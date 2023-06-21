1652cb2bb6da7f16a8542af700647ad612e306a6
a.drobysheva <a.drobysheva@flash.ru>
2023-06-21 10:18:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+/*
+Console.WriteLine("Первое число ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+if (numb1 > numb2)
+{
+    Console.WriteLine("a = " + numb1 + "; b = " + numb2 + " -> " + "max = " + numb1);
+}
+else
+{
+    Console.WriteLine("a = " + numb1 + "; b = " + numb2 + " -> " + "max = " + numb2);
+}
+*/
+
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+/*
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a % 2 == 0 ? "Четное" : "Не четное");
+*/
